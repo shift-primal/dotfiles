f20a6236472e0e405581f2c6caa63c960546f94e
Kasper Haugestøl <kasper@haugestol.com>
2026-02-24 22:23:29 +0100

    Rework Done!

--- user_scripts/etc/project-manager.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=~/Documents/OtherProjects/expenses_tracker
-
-kitty --detach --working-directory=$DIR sh -c "nvim plan.md"
-kitty --detach --working-directory=$DIR sh -c "nvim ."
-kitty --detach --working-directory=$DIR sh -c "claude -c"
-kitty --detach --working-directory=$DIR/backend
