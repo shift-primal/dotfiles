2751a2f4a0c4438d874bf34646a5d8e22b1c1449
Kasper Haugestøl <kasper@haugestol.com>
2026-04-15 23:17:57 +0200

    revamp -> noctalia

--- dot_config/nvim/lsp/clangd.lua
+++
@@ -1,5 +0,0 @@
-return {
-	cmd = { 'clangd', '--background-index', '--clang-tidy', '--header-insertion=iwyu' },
-	filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
-	root_markers = { 'compile_commands.json', 'compile_flags.txt', '.clangd', '.git' },
-}
